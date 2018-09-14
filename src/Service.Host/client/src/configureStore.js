dd6c200cde672003b941944f8ebb010f8823f287
richard phillips <richard.phillips@linn.co.uk>
2018-09-14 16:09:07 +0100

    first commit

---
+++ src/Service.Host/client/src/configureStore.js
@@ -0,0 +1,25 @@
+﻿import { createStore, applyMiddleware, compose } from 'redux';
+import { routerMiddleware } from 'react-router-redux';
+import { apiMiddleware as api } from 'redux-api-middleware';
+import thunkMiddleware from 'redux-thunk';
+import history from './history';
+import reducer from './reducers';
+import authorization from './middleware/authorization';
+
+const composeEnhancers = window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] || compose;
+
+const middleware = [
+    authorization,
+    api,
+    thunkMiddleware,
+    routerMiddleware(history)
+];
+
+const configureStore = initialState => {
+    const enhancers = composeEnhancers(applyMiddleware(...middleware));
+    const store = createStore(reducer, initialState, enhancers);
+
+    return store;
+};
+
+export default configureStore;
