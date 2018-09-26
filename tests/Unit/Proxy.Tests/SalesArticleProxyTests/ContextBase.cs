4b6e66471184ff72f575da623c19e042abc97e71
richard phillips <richard.phillips@linn.co.uk>
2018-09-26 16:44:17 +0100

    add test for proxy that may remain after repositories added.

---
+++ tests/Unit/Proxy.Tests/SalesArticleProxyTests/ContextBase.cs
@@ -0,0 +1,25 @@
+﻿namespace Linn.Products.Proxy.Tests.SalesArticleProxyTests
+{
+    using Linn.Common.Proxy;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public abstract class ContextBase
+    {
+        protected SalesArticleProxy Sut { get; private set; }
+
+        protected IRestClient RestClient { get; private set; }
+
+        protected string ProxyRoot { get; private set; }
+
+        [SetUp]
+        public void EstablishContext()
+        {
+            this.ProxyRoot = "http://app.linn.co.uk";
+            this.RestClient = Substitute.For<IRestClient>();
+            this.Sut = new SalesArticleProxy(this.RestClient, this.ProxyRoot);
+        }
+    }
+}
