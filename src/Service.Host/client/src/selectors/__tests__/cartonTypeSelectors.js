6f19c9ed7730c64ce4e935660eed106fb3ec5cb2
richard phillips <richard.phillips@linn.co.uk>
2018-10-16 15:38:03 +0100

    carton type selectors

---
+++ src/Service.Host/client/src/selectors/__tests__/cartonTypeSelectors.js
@@ -0,0 +1,76 @@
+﻿import { getCartonType, getCartonLoading, getCartonEditStatus } from '../cartonTypeSelectors';
+
+describe('when getting carton type', () => {
+    test('should return carton', () => {
+
+        const state = {
+            cartonType: {
+                item: { name: 'name' }
+            }
+        };
+
+        const expectedResult = { name: 'name' };
+
+        expect(getCartonType(state)).toEqual(expectedResult);
+    });
+});
+
+describe('when getting no carton type', () => {
+    test('should return null', () => {
+
+        const state = {
+            cartonType: {
+                item: null
+            }
+        };
+
+        expect(getCartonType(state)).toEqual(null);
+    });
+});
+
+describe('when getting loading', () => {
+    test('should return true', () => {
+
+        const state = {
+            cartonType: {
+                item: null,
+                loading: true
+            }
+        };
+
+        expect(getCartonLoading(state)).toEqual(true);
+    });
+});
+
+describe('when getting edit status', () => {
+    test('should return status', () => {
+
+        const state = {
+            cartonType: {
+                item: { name: 'name' },
+                loading: false,
+                editStatus: 'create'
+            }
+        };
+
+        const expectedResult = 'create';
+
+        expect(getCartonEditStatus(state)).toEqual(expectedResult);
+    });
+});
+
+describe('when getting no edit status', () => {
+    test('should return view', () => {
+
+        const state = {
+            cartonType: {
+                item: { name: 'name' },
+                loading: false
+            }
+        };
+
+        const expectedResult = 'view';
+
+        expect(getCartonEditStatus(state)).toEqual(expectedResult);
+    });
+});
---
+++ src/Service.Host/client/src/selectors/cartonTypeSelectors.js
@@ -0,0 +1,34 @@
+﻿export const getCartonType = (state) => {
+    const cartonType = state.cartonType;
+    if (!cartonType) {
+        return null;
+    }
+
+    if (cartonType.item) {
+        return cartonType.item;
+    }
+
+    return null;
+}
+
+export const getCartonLoading = (state) => {
+    const cartonType = state.cartonType;
+    if (!cartonType) {
+        return null;
+    }
+
+    return cartonType.loading;
+}
+
+export const getCartonEditStatus = (state) => {
+    const cartonType = state.cartonType;
+    if (!cartonType) {
+        return null;
+    }
+
+    if (!cartonType.editStatus) {
+        return 'view';
+    }
+
+    return cartonType.editStatus;
+}
