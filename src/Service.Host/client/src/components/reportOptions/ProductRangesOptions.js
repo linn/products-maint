9c858521e0f8103db5589eb055c59498d5bdbaad
Lewis Renfrew <LewisRe@linn.co.uk>
2019-01-07 09:48:38 +0000

    reports and options components covnerted to material ui. Don't look great at the moment but I'll style once we decide what to do about css

--- src/Service.Host/client/src/components/reportOptions/ProductRangesOptions.js
+++ src/Service.Host/client/src/components/reportOptions/ProductRangesOptions.js
@@ -1,5 +1,17 @@
 ﻿import React, { Component } from 'react';
-import { PageHeader, Grid, Checkbox, Row, Col, Button } from 'react-bootstrap';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        paddingLeft: "20%",
+        paddingRight: "20%",
+    },
+});
 
 class ProductRangesOptions extends Component {
     constructor(props) {
@@ -25,31 +37,31 @@ class ProductRangesOptions extends Component {
     }
 
     render() {
+        const {classes } = this.props;
         return (
-            <div className="container">
-                <Grid>
-                    <PageHeader>
-                        <small>Product Ranges Report Options</small>
-                    </PageHeader>
-
-                    <Row>
-                        <Col xs={10}>
-                            <Checkbox checked={this.state.includePhasedOut} onChange={ch => this.handlePhaseOutChange(ch.target.checked)}>
-                                Include phased out
-                            </Checkbox>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={10}>
-                            <Button bsClass="btn" onClick={() => this.handleClick()}>
-                                Run Report
-                            </Button>
-                        </Col>
-                    </Row>
+            <div className={classes.root}>
+                <Grid container spacing={24} justify="center">
+                    <Grid item xs={12}>
+                        <h4>Ean Codes of Sales Articles Report Options</h4>
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    checked={this.state.includePhasedOut}
+                                    onChange={ch => this.handlePhaseOutChange(ch.target.checked)}
+                                    value={this.state.includePhasedOut}
+                                    color="primary"
+                                />
+                            }
+                            label="Include phased out"
+                        />
+                    </Grid>
+                    <Button variant="outlined" bsClass="btn" onClick={() => this.handleClick()}>
+                        Run Report
+                    </Button>
                 </Grid>
             </div>
         );
     }
 }
 
-export default ProductRangesOptions;
+export default withStyles(styles)(ProductRangesOptions);
