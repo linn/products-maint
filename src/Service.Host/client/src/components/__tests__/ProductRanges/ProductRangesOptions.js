90ebef19b25dc27946d4f443f469e410f7f5842a
Lewis Renfrew <LewisRe@linn.co.uk>
2019-01-07 15:56:35 +0000

    material ui versions of reports and report options

---
+++ src/Service.Host/client/src/components/__tests__/ProductRanges/ProductRangesOptions.js
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import { createShallow} from '@material-ui/core/test-utils';
+import ProductRangesOptions from '../../reportOptions/ProductRangesOptions';
+
+describe('<ProductRangesOptions />', () => {
+    const getButton = () => wrapper.find('WithStyles(Button)');
+    const shallow = createShallow({ dive: true });
+
+    let wrapper, history;
+
+    beforeEach(() => { 
+        history = { push: jest.fn() }; 
+        wrapper = shallow(<ProductRangesOptions history={history}  />)
+    });
+
+    test('Should redirect with state parameters when button clicked', () => {
+        wrapper.setState({ includePhasedOut: true });
+        getButton().simulate("click");
+        expect(history.push).toHaveBeenCalledWith({
+            pathname: '/products/reports/product-ranges/report',
+            search: "?includePhasedOut=true"
+        });
+    });
+});
