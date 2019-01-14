1b834551f80a882625f6dfce6712b009cd908d25
Adam Campbell <Adam.Campbell@linn.co.uk>
2019-01-14 16:37:42 +0000

    add service

---
+++ src/Service/ResponseProcessors/TypesOfSaleResponseProcessor.cs
@@ -0,0 +1,16 @@
+﻿namespace Linn.Products.Service.ResponseProcessors
+{
+    using System.Collections.Generic;
+
+    using Linn.Common.Facade;
+    using Linn.Common.Nancy.Facade;
+    using Linn.Products.Domain.Linnapps;
+
+    public class TypesOfSaleResponseProcessor : JsonResponseProcessor<IEnumerable<TypeOfSale>>
+    {
+        public TypesOfSaleResponseProcessor(IResourceBuilder<IEnumerable<TypeOfSale>> resourceBuilder)
+            : base(resourceBuilder, "types-of-sale", 1)
+        {
+        }
+    }
+}
---
+++ tests/Integration/Service.Tests/TypeOfSaleModuleSpecs/WhenUpdatingTypeOfSale.cs
@@ -0,0 +1,65 @@
+﻿namespace Linn.Products.Service.Tests.TypeOfSaleModuleSpecs
+{
+    using FluentAssertions;
+
+    using Linn.Common.Facade;
+    using Linn.Products.Domain.Linnapps;
+    using Linn.Products.Resources;
+
+    using Nancy;
+    using Nancy.Testing;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class WhenUpdatingTypeOfSale : ContextBase
+    {
+        private TypeOfSaleResource requestResource;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.requestResource = new TypeOfSaleResource
+                                       {
+                                           Description = "new desc",
+                                           Department = "new dept",
+                                           Nominal = "new nom",
+                                           RealSale = "N"
+                                       };
+            var typeOfSale = new TypeOfSale("name", "desc", "nom", "dept", "Y") { Description = "new desc" };
+            this.TypeOfSaleService.Update("name", Arg.Any<TypeOfSaleResource>()).Returns(new SuccessResult<TypeOfSale>(typeOfSale) { Data = typeOfSale });
+
+            this.Response = this.Browser.Put(
+                "/products/maint/types-of-sale/name",
+                with =>
+                    {
+                        with.Header("Accept", "application/json");
+                        with.Header("Content-Type", "application/json");
+                        with.JsonBody(this.requestResource);
+                    }).Result;
+        }
+
+        [Test]
+        public void ShouldReturnOk()
+        {
+            this.Response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Test]
+        public void ShouldCallService()
+        {
+            this.TypeOfSaleService.Received().Update(
+                "name",
+                Arg.Is<TypeOfSaleResource>(r => r.Description == this.requestResource.Description));
+        }
+
+        [Test]
+        public void ShouldReturnResource()
+        {
+            var resource = this.Response.Body.DeserializeJson<TypeOfSaleResource>();
+            resource.Name.Should().Be("name");
+            resource.Description.Should().Be("new desc");
+        }
+    }
+}
