6d1d092cbd6b9e49e9b8374caa631ca90808cc7d
Andrew Fraser <andrewfraser73@hotmail.com>
2019-01-24 15:52:25 +0000

    change timezone to utc from gmt

--- src/Persistence.Linnapps/ServiceDbContext.cs
+++ src/Persistence.Linnapps/ServiceDbContext.cs
@@ -29,7 +29,7 @@
             var userId = ConfigurationManager.Configuration["DATABASE_USER_ID"];
             var password = ConfigurationManager.Configuration["DATABASE_PASSWORD"];
             var serviceId = ConfigurationManager.Configuration["DATABASE_NAME"];
-            var tz = ConfigurationManager.Configuration["TIMEZONE"];
+            var timezone = ConfigurationManager.Configuration["TIMEZONE"];
 
             var dataSource = $"(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST={host})(PORT=1521))(CONNECT_DATA=(SERVICE_NAME={serviceId})(SERVER=dedicated)))";
 
