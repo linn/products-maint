3fea52dc1345712a745807681e1e2d4254d6813a
Andrew Fraser <andrewfraser73@hotmail.com>
2019-01-24 16:43:03 +0000

    cement TZ in dockerfile for funzies

--- src/Persistence.Linnapps/ServiceDbContext.cs
+++ src/Persistence.Linnapps/ServiceDbContext.cs
@@ -29,7 +29,6 @@
             var userId = ConfigurationManager.Configuration["DATABASE_USER_ID"];
             var password = ConfigurationManager.Configuration["DATABASE_PASSWORD"];
             var serviceId = ConfigurationManager.Configuration["DATABASE_NAME"];
-            var timezone = ConfigurationManager.Configuration["TIMEZONE"];
 
             var dataSource = $"(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST={host})(PORT=1521))(CONNECT_DATA=(SERVICE_NAME={serviceId})(SERVER=dedicated)))";
 
