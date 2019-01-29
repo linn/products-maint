e140964eeba4edf5194de6eca226c680bf3efaf9
Lewis Renfrew <LewisRe@linn.co.uk>
2019-01-29 16:52:06 +0000

    ui tweaks

--- src/Service.Host/client/src/components/reports/StockTriggerLevelParts.js
+++ src/Service.Host/client/src/components/reports/StockTriggerLevelParts.js
@@ -1,38 +1,46 @@
 import React, { Component } from 'react';
 import { Loading } from '@linn-it/linn-form-components-library';
-import { Grid } from '@material-ui/core';
+import { Grid, Paper } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
-import {ReportTable } from '@linn-it/linn-form-components-library';
+import { ReportTable, Title } from '@linn-it/linn-form-components-library';
 
 const styles = theme => ({
     root: {
-      paddingLeft: "1%",
-      paddingRight: "1%",
-      
+        paddingLeft: "1%",
+        paddingRight: "1%",
+        paddingTop: "30px",
+        paddingBottom: "30px"
     },
+    message: {
+        paddingTop: "100px",
+        paddingBottom: "100px"
+    }
 });
 
 class StockTriggerLevelParts extends Component {
     render() {
-        const { reportData, loading,  classes } = this.props;
+        const { reportData, loading, classes } = this.props;
         return (
-            <div className={classes.root}>
+            <Paper className={classes.root}>
                 <Grid container spacing={24} justify="center">
-                    <Grid item xs={12}>   
+                    <Grid item xs={12}>
+                        <Title text={reportData ? reportData.title.displayString : null} />
                     </Grid>
                     <Grid item xs={12}>
-                    { !reportData ? <Loading /> :
-                        reportData.results.length > 0 ?
-                        <ReportTable 
-                            reportData={reportData} 
-                            showTotals={false} 
-                            showRowTitles={true}
-                            title={reportData ? reportData.title.displayString : null}
-                            showTitle={true} 
-                    /> : <ErrorCard errorMessage="No Stock Found" className={classes.message}  /> }
+                        {!reportData ? <Loading /> :
+                            reportData.results.length > 0 ?
+                                <ReportTable
+                                    reportData={reportData}
+                                    showTotals={false}
+                                    showRowTitles={true}
+                                    title={reportData ? reportData.title.displayString : null}
+                                    showTitle={false}
+                                /> : <Paper className={classes.message}>
+                                    <ErrorCard errorMessage="No Stock Found" />
+                                </Paper>}
                     </Grid>
                 </Grid>
-            </div>
+            </Paper>
         );
     }
 }
--- src/Service.Host/client/src/components/reports/StockTriggerLevelsByPart.js
+++ src/Service.Host/client/src/components/reports/StockTriggerLevelsByPart.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import { Loading } from '@linn-it/linn-form-components-library';
-import { Grid } from '@material-ui/core';
+import { Grid, Paper } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
-import {ReportTable } from '@linn-it/linn-form-components-library';
+import { ReportTable } from '@linn-it/linn-form-components-library';
 import { BackButton } from '@linn-it/linn-form-components-library';
-import { ErrorCard } from '@linn-it/linn-form-components-library';
+import { ErrorCard, Title } from '@linn-it/linn-form-components-library';
 
 const styles = theme => ({
     root: {
-      paddingLeft: "1%",
-      paddingRight: "1%",
+        paddingLeft: "1%",
+        paddingRight: "1%",
+        paddingTop: "30px",
+        paddingBottom: "30px"
     },
     message: {
-        paddingTop: "30px"
+        paddingTop: "100px",
+        paddingBottom: "100px"
     }
 });
 
@@ -25,29 +28,33 @@ class StockTriggerLevelsByPart extends Component {
     }
 
     render() {
-        const { reportData, loading,  classes} = this.props;
-        
+        const { reportData, loading, classes } = this.props;
+
         return (
-            <div className={classes.root}>
+            <Paper className={classes.root}>
                 <Grid container spacing={24} justify="center">
-                    <Grid item xs={12}>  
+                    <Grid item xs={12}>
+                        <Title text={reportData ? reportData.title.displayString : null} />
                     </Grid>
                     <Grid item xs={12}>
-                        { !reportData ? <Loading /> :
-                        reportData.results.length > 0 ?
-                        <ReportTable 
-                            reportData={reportData} 
-                            showTotals={false} 
-                            showRowTitles={false}
-                            title={reportData ? reportData.title.displayString : null}
-                            showTitle={true} 
-                        /> : <ErrorCard errorMessage="No Stock Found" className={classes.message}  /> }
+                        <BackButton backClick={this.handleBackClick} />
                     </Grid>
-                    <Grid item xs={12}>   
-                        <BackButton backClick ={this.handleBackClick} />
+                    <Grid item xs={12}>
+                        {!reportData ? <Loading /> :
+                            reportData.results.length > 0 ?
+                                <ReportTable
+                                    reportData={reportData}
+                                    showTotals={false}
+                                    showRowTitles={false}
+                                    title={reportData ? reportData.title.displayString : null}
+                                    showTitle={false}
+                                /> : <Paper className={classes.message}>
+                                    <ErrorCard errorMessage="No Stock Found" className={classes.message} />
+                                </Paper>}
                     </Grid>
+
                 </Grid>
-            </div>
+            </Paper>
         );
     }
 }
