0dfe12f18f01ce847dcd7c7bd1d226aa7da0072a
Lewis Renfrew <LewisRe@linn.co.uk>
2019-02-11 12:29:43 +0000

    new nav idea

---
+++ src/Service.Host/client/src/components/Menu.js
@@ -0,0 +1,127 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Divider from '@material-ui/core/Divider';
+import { List, ListItem, Typography } from '@material-ui/core';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { createMuiTheme, withStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
+import Page from '../containers/Page';
+
+const styles = theme => ({
+    fragment: {
+        overflow: 'hidden'
+    },
+    root: {
+        width: '100%',
+        backgroundColor: theme.palette.background.paper,
+        position: 'relative',
+        overflow: 'auto'
+    },
+    listSection: {
+        backgroundColor: 'inherit',
+        margin: '10px'
+    },
+    ul: {
+        backgroundColor: 'inherit',
+        padding: 0
+    },
+    innerList: {
+        margin: '20px'
+    },
+    subHeaders: {
+        fontSize: '18px',
+        fontWeight: 1000,
+        color: 'black'
+    }
+});
+
+const theme = createMuiTheme({
+    overrides: {
+        MuiListSubheader: {
+            root: {
+                fontSize: '32px',
+                color: 'black',
+                padding: '15px'
+            }
+        },
+        MuiListItem: {
+            root: {
+                paddingTop: '0px',
+                paddingBottom: '0px'
+            }
+        }
+    }
+});
+
+const menu = require('./menu.json').sections; // should get this via GET request in real life
+
+const PinnedSubheaderList = ({ classes, list }) => (
+    <List className={classes.root} subheading={<li />}>
+        {list.map(item => (
+            <li key={item.title} className={classes.listSection}>
+                <ul className={classes.ul}>
+                    <ListSubheader>
+                        {item.title} <Divider />
+                    </ListSubheader>
+                    <div className={classes.innerList}>
+                        {item.items.map(entry => (
+                            <Link to={entry.href} key={entry.title}>
+                                <ListItem>
+                                    <Typography color="primary"> {entry.title} </Typography>
+                                </ListItem>{' '}
+                            </Link>
+                        ))}
+                    </div>
+                </ul>
+            </li>
+        ))}
+    </List>
+);
+
+class MenuPage extends Component {
+    getMenu() {
+        const { match } = this.props;
+        const id = match.params.sectionId;
+        const section = menu.filter(x => x.id === id);
+        const { columns } = section[0];
+        const categoriesListArray = [];
+
+        columns.forEach(column => {
+            categoriesListArray.push(column.categories);
+        });
+
+        const lists = [];
+
+        categoriesListArray.forEach(categoriesList => {
+            lists.push(categoriesList);
+        });
+
+        // lists is an array of arrays at this point, we want a single array
+        const singleArrayList = [];
+        lists.forEach(list => {
+            list.forEach(item => {
+                singleArrayList.push(item);
+            });
+        });
+        return singleArrayList;
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <Page>
+                <MuiThemeProvider theme={theme}>
+                    <PinnedSubheaderList list={this.getMenu()} classes={classes} />
+                </MuiThemeProvider>
+            </Page>
+        );
+    }
+}
+
+MenuPage.propTypes = {
+    match: PropTypes.shape({}).isRequired,
+    classes: PropTypes.shape({}).isRequired
+};
+
+export default withStyles(styles)(MenuPage);
--- src/Service.Host/client/src/components/Root.js
+++ src/Service.Host/client/src/components/Root.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { Route, Redirect, Switch } from 'react-router';
 import { OidcProvider } from 'redux-oidc';
 import { Router } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import history from '../history';
-import Navigation from './Navigation';
+import Navigation from '../containers/Navigation';
+import MenuPage from './Menu';
 import App from './App';
 import Callback from '../containers/Callback';
 import Tariff from '../containers/Tariff';
@@ -35,84 +36,185 @@ import 'typeface-roboto';
 import SalesArticles from '../containers/salesArticles/SalesArticles';
 import SalesArticle from '../containers/salesArticles/SalesArticle';
 
-class Root extends Component {
-    render() {
-        const { store } = this.props;
-
-        return (
-            <div>
-            <Navigation />
-            <div style={{paddingTop: "40px"}}>
+const Root = ({ store }) => (
+    <div>
+        <div style={{ paddingTop: '40px' }}>
             <Provider store={store}>
                 <OidcProvider store={store} userManager={userManager}>
-             
-               
                     <Router history={history}>
                         <div>
-                           
+                            <Navigation />
                             <CssBaseline />
 
-                            <Route path="/" render={() => { document.title = 'Products'; return false; }} />
-                            <Route exact path="/" render={() => <Redirect to="/products/maint" />} />
+                            <Route
+                                path="/"
+                                render={() => {
+                                    document.title = 'Products';
+                                    return false;
+                                }}
+                            />
+                            <Route
+                                exact
+                                path="/"
+                                render={() => <Redirect to="/products/maint" />}
+                            />
                             <Route exact path="/products/maint" component={App} />
-                            <Route exact path="/products/maint/signin-oidc-client" component={Callback} />
+                            <Route
+                                exact
+                                path="/products/maint/signin-oidc-client"
+                                component={Callback}
+                            />
 
                             <Switch>
-                                <Route exact path="/products/maint/tariffs/:id" component={Tariff} />
+                                <Route
+                                    exact
+                                    path="/products/maint/tariffs/:id"
+                                    component={Tariff}
+                                />
                                 <Route exact path="/products/maint/tariffs" component={Tariffs} />
                             </Switch>
 
                             <Route exact path="/products/reports" component={App} />
-                            <Route exact path="/products/reports/sales-article-ean-codes" component={EanCodesOptions} />
-                            <Route exact path="/products/reports/sales-article-ean-codes/report" component={EanCodes} />
+                            <Route
+                                exact
+                                path="/products/reports/sales-article-ean-codes"
+                                component={EanCodesOptions}
+                            />
+                            <Route
+                                exact
+                                path="/products/reports/sales-article-ean-codes/report"
+                                component={EanCodes}
+                            />
 
-                            <Route exact path="/products/reports/carton-details" component={CartonDetailsOptions} />
-                            <Route exact path="/products/reports/carton-details/report" component={CartonDetails} />
+                            <Route
+                                exact
+                                path="/products/reports/carton-details"
+                                component={CartonDetailsOptions}
+                            />
+                            <Route
+                                exact
+                                path="/products/reports/carton-details/report"
+                                component={CartonDetails}
+                            />
 
                             <Switch>
-                                <Route exact path="/products/maint/sales-articles" component={SalesArticles} />
-                                <Route exact path="/products/maint/sales-articles/:articleNumber" component={SalesArticle} />
+                                <Route
+                                    exact
+                                    path="/products/maint/sales-articles"
+                                    component={SalesArticles}
+                                />
+                                <Route
+                                    exact
+                                    path="/products/maint/sales-articles/:articleNumber"
+                                    component={SalesArticle}
+                                />
                             </Switch>
 
                             <Switch>
-                                <Route exact path="/products/maint/carton-types/create" component={CreateCartonType} />
-                                <Route exact path="/products/maint/carton-types/:cartonTypeId" component={CartonType} />
-                                <Route exact path="/products/maint/carton-types" component={CartonDetails} />
+                                <Route
+                                    exact
+                                    path="/products/maint/carton-types/create"
+                                    component={CreateCartonType}
+                                />
+                                <Route
+                                    exact
+                                    path="/products/maint/carton-types/:cartonTypeId"
+                                    component={CartonType}
+                                />
+                                <Route
+                                    exact
+                                    path="/products/maint/carton-types"
+                                    component={CartonDetails}
+                                />
                             </Switch>
 
                             <Switch>
-                                <Route exact path="/products/maint/sernos-configs" component={SernosConfigs} />
-                                <Route exact path="/products/maint/sernos-configs/create" component={CreateSernosConfig} />
-                                <Route exact path="/products/maint/sernos-configs/:sernosConfigId" component={SernosConfig} />
+                                <Route
+                                    exact
+                                    path="/products/maint/sernos-configs"
+                                    component={SernosConfigs}
+                                />
+                                <Route
+                                    exact
+                                    path="/products/maint/sernos-configs/create"
+                                    component={CreateSernosConfig}
+                                />
+                                <Route
+                                    exact
+                                    path="/products/maint/sernos-configs/:sernosConfigId"
+                                    component={SernosConfig}
+                                />
                             </Switch>
 
                             <Switch>
-                                <Route exact path="/products/maint/sa-core-types" component={SaCoreTypes} />
-                                <Route exact path="/products/maint/sa-core-types/create" component={CreateSaCoreType} />
-                                <Route exact path="/products/maint/sa-core-types/:coreType" component={SaCoreType} />
+                                <Route
+                                    exact
+                                    path="/products/maint/sa-core-types"
+                                    component={SaCoreTypes}
+                                />
+                                <Route
+                                    exact
+                                    path="/products/maint/sa-core-types/create"
+                                    component={CreateSaCoreType}
+                                />
+                                <Route
+                                    exact
+                                    path="/products/maint/sa-core-types/:coreType"
+                                    component={SaCoreType}
+                                />
                             </Switch>
 
                             <Switch>
-                                <Route exact path="/products/maint/types-of-sale" component={TypesOfSale} />
-                                <Route exact path="/products/maint/types-of-sale/create" component={CreateTypeOfSale} />
-                                <Route exact path="/products/maint/types-of-sale/:typeOfSaleId" component={TypeOfSale} />
+                                <Route
+                                    exact
+                                    path="/products/maint/types-of-sale"
+                                    component={TypesOfSale}
+                                />
+                                <Route
+                                    exact
+                                    path="/products/maint/types-of-sale/create"
+                                    component={CreateTypeOfSale}
+                                />
+                                <Route
+                                    exact
+                                    path="/products/maint/types-of-sale/:typeOfSaleId"
+                                    component={TypeOfSale}
+                                />
                             </Switch>
 
-                            <Route exact path="/products/reports/product-ranges" component={ProductRangesOptions} />
-                            <Route exact path="/products/reports/product-ranges/report" component={ProductRanges} />
-                            <Route exact path="/products/reports/sales-products-by-product-range" component={SalesProductsByProductRange} />
+                            <Route
+                                exact
+                                path="/products/reports/product-ranges"
+                                component={ProductRangesOptions}
+                            />
+                            <Route
+                                exact
+                                path="/products/reports/product-ranges/report"
+                                component={ProductRanges}
+                            />
+                            <Route
+                                exact
+                                path="/products/reports/sales-products-by-product-range"
+                                component={SalesProductsByProductRange}
+                            />
 
-                            <Route exact path="/products/reports/parts-at-location/:locationId" component={StockTriggerLevelParts} />
-                            <Route exact path="/products/reports/stock-trigger-levels/:locationId/:partNumber" component={StockTriggerLevelsByPart} />
+                            <Route
+                                exact
+                                path="/products/reports/parts-at-location/:locationId"
+                                component={StockTriggerLevelParts}
+                            />
+                            <Route
+                                exact
+                                path="/products/reports/stock-trigger-levels/:locationId/:partNumber"
+                                component={StockTriggerLevelsByPart}
+                            />
+                            <Route exact path="/:sectionId" component={MenuPage} />
                         </div>
                     </Router>
-                
                 </OidcProvider>
-                
-            </Provider> </div> </div>
-        );
-
-    }
-}
+            </Provider>{' '}
+        </div>{' '}
+    </div>
+);
 
 export default Root;
