68adf8f6175f00faca95fb2ebf9161f3aa655fb3
Adam Campbell <Adam.Campbell@linn.co.uk>
2019-02-13 10:06:27 +0000

    add domain

---
+++ src/Domain.Linnapps/VATCode.cs
@@ -0,0 +1,28 @@
+﻿namespace Linn.Products.Domain.Linnapps
+{
+    public class VatCode
+    {
+        public VatCode(string code, string description, double rate, string reason)
+        {
+            this.Code = code;
+            this.Description = description;
+            this.Rate = rate;
+            this.Reason = reason;
+        }
+
+        public string Code { get; private set; }
+
+        public string Description { get; set; }
+
+        public double Rate { get; set; }
+
+        public string Reason { get; set; }
+
+        public void Update(string description, double rate, string reason = null)
+        {
+            this.Description = description;
+            this.Rate = rate;
+            this.Reason = reason;
+        }
+    }
+}
---
+++ tests/Unit/Domain.Linnapps.Tests/VatCodeSpecs/WhenUpdatingVatCode.cs
@@ -0,0 +1,23 @@
+﻿namespace Domain.Linnapps.Tests.VatCodeSpecs
+{
+    using FluentAssertions;
+
+    using NUnit.Framework;
+
+    public class WhenUpdatingVatCode : ContextBase
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            this.Sut.Update("UK VAT ZERO RATE", 0, "REASON");
+        }
+
+        [Test]
+        public void ShouldUpdate()
+        {
+            this.Sut.Description.Should().Be("UK VAT ZERO RATE");
+            this.Sut.Rate.Should().Be(0);
+            this.Sut.Reason.Should().Be("REASON");
+        }
+    }
+}
