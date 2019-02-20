edad452b3453f257200983f5da96fd4712012a22
Adam Campbell <Adam.Campbell@linn.co.uk>
2019-02-20 16:39:33 +0000

    domain

---
+++ src/Domain.Linnapps/SernosSequence.cs
@@ -0,0 +1,30 @@
+﻿namespace Linn.Products.Domain.Linnapps
+{
+    using System;
+
+    public class SernosSequence
+    {
+        public SernosSequence(string sequenceName, string description, int nextSerialNumber, DateTime? dateClosed)
+        {
+            this.SequenceName = sequenceName;
+            this.Description = description;
+            this.NextSerialNumber = nextSerialNumber;
+            this.DateClosed = dateClosed;
+        }
+
+        public DateTime? DateClosed { get; set; }
+
+        public string Description { get; set; }
+
+        public int NextSerialNumber { get; set; }
+
+        public string SequenceName { get; set; }
+
+        public void Update(string description, int nextSerialNumber, DateTime? dateClosed)
+        {
+            this.Description = description;
+            this.NextSerialNumber = nextSerialNumber;
+            this.DateClosed = dateClosed;
+        }
+    }
+}
---
+++ tests/Unit/Domain.Linnapps.Tests/SernosSequenceSpecs/WhenUpdatingSernosSequence.cs
@@ -0,0 +1,24 @@
+﻿namespace Linn.Products.Domain.Linnapps.Tests.SernosSequenceSpecs
+{
+    using FluentAssertions;
+    using FluentAssertions.Extensions;
+
+    using NUnit.Framework;
+
+    public class WhenUpdatingSernosSequence : ContextBase
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            this.Sut.Update("CD MECHANISM", 501, 20.February(2019));
+        }
+
+        [Test]
+        public void ShouldUpdate()
+        {
+            this.Sut.Description.Should().Be("CD MECHANISM");
+            this.Sut.NextSerialNumber.Should().Be(501);
+            this.Sut.DateClosed.Should().Be(20.February(2019));
+        }
+    }
+}
