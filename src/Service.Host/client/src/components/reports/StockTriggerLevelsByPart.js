9d1b592ba8a1b710ef56d33f041ed5db7a21558b
richard phillips <richard.phillips@linn.co.uk>
2019-03-28 11:13:18 +0000

    remove some linting complaints.

--- src/Service.Host/client/src/components/reports/StockTriggerLevelsByPart.js
+++ src/Service.Host/client/src/components/reports/StockTriggerLevelsByPart.js
@@ -1,36 +1,38 @@
 import React, { Component } from 'react';
-import { Loading } from '@linn-it/linn-form-components-library';
+import {
+    Loading,
+    ErrorCard,
+    Title,
+    BackButton,
+    ReportTable
+} from '@linn-it/linn-form-components-library';
 import { Grid, Paper } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
-import { ReportTable } from '@linn-it/linn-form-components-library';
-import { BackButton } from '@linn-it/linn-form-components-library';
-import { ErrorCard, Title } from '@linn-it/linn-form-components-library';
+import PropTypes from 'prop-types';
 
-const styles = theme => ({
+const styles = () => ({
     root: {
         margin: 40,
         padding: 40,
-        paddingTop: "30px",
-        paddingBottom: "30px"
+        paddingTop: '30px',
+        paddingBottom: '30px'
     },
-    
     message: {
-        paddingTop: "100px",
-        paddingBottom: "100px"
+        paddingTop: '100px',
+        paddingBottom: '100px'
     }
 });
 
 class StockTriggerLevelsByPart extends Component {
-
     handleBackClick = () => {
-        const { history } = this.props;
-        let locationId = this.props.match.params.locationId;
+        const { history, match } = this.props;
+        const { locationId } = match.params;
 
-        history.push('/products/reports/parts-at-location/' + locationId);
-    }
+        history.push(`/products/reports/parts-at-location/${locationId}`);
+    };
 
     render() {
-        const { reportData, loading, classes } = this.props;
+        const { reportData, classes } = this.props;
 
         return (
             <Paper className={classes.root}>
@@ -42,23 +44,41 @@ class StockTriggerLevelsByPart extends Component {
                         <BackButton backClick={this.handleBackClick} />
                     </Grid>
                     <Grid item xs={12}>
-                        {!reportData ? <Loading /> :
-                            reportData.results.length > 0 ?
-                                <ReportTable
-                                    reportData={reportData}
-                                    showTotals={false}
-                                    showRowTitles={false}
-                                    title={reportData ? reportData.title.displayString : null}
-                                    showTitle={false}
-                                /> : <Paper className={classes.message}>
-                                    <ErrorCard errorMessage="No Stock Found" className={classes.message} />
-                                </Paper>}
+                        {!reportData ? (
+                            <Loading />
+                        ) : reportData.results.length > 0 ? (
+                            <ReportTable
+                                reportData={reportData}
+                                showTotals={false}
+                                showRowTitles={false}
+                                title={reportData.title.displayString}
+                                showTitle={false}
+                            />
+                        ) : (
+                            <Paper className={classes.message}>
+                                <ErrorCard
+                                    errorMessage="No Stock Found"
+                                    className={classes.message}
+                                />
+                            </Paper>
+                        )}
                     </Grid>
-
                 </Grid>
             </Paper>
         );
     }
 }
+
+StockTriggerLevelsByPart.propTypes = {
+    reportData: PropTypes.shape({}),
+    classes: PropTypes.shape({}),
+    match: PropTypes.shape({}).isRequired,
+    history: PropTypes.shape({ push: PropTypes.func }).isRequired
+};
+
+StockTriggerLevelsByPart.defaultProps = {
+    reportData: null,
+    classes: {}
+};
 
 export default withStyles(styles)(StockTriggerLevelsByPart);
