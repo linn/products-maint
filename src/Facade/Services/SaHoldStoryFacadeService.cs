cee64b43b87344313c0b6099cd1be97bc83ca266
lewisre <lewisre@PC1152>
2019-04-03 09:15:28 +0100

    facade service, employee repository

--- src/Facade/Services/SaHoldStoryFacadeService.cs
+++ src/Facade/Services/SaHoldStoryFacadeService.cs
@@ -1,23 +1,55 @@
 ﻿namespace Linn.Products.Facade.Services
 {
     using System;
+    using System.Linq;
     using System.Linq.Expressions;
+    using System.Reflection.Metadata.Ecma335;
 
     using Linn.Common.Facade;
     using Linn.Common.Persistence;
     using Linn.Products.Domain.Linnapps;
+    using Linn.Products.Domain.Linnapps.Products;
+    using Linn.Products.Domain.Linnapps.RemoteServices;
+    using Linn.Products.Facade.Extensions;
+    using Linn.Products.Persistence.Linnapps.Repositories;
     using Linn.Products.Resources;
 
     public class SaHoldStoryFacadeService : FacadeService<SaHoldStory, int, SaHoldStoryResource, SaHoldStoryResource>
     {
-        public SaHoldStoryFacadeService(IRepository<SaHoldStory, int> repo, ITransactionManager transactionManager)
+        private readonly IRepository<SalesArticle, string> salesArticleRepository;
+
+        private IRepository<Employee, int> employeeRepository;
+
+        public SaHoldStoryFacadeService(
+            IRepository<SaHoldStory, int> repo,
+            ITransactionManager transactionManager,
+            IRepository<SalesArticle, string> salesArticleRepository,
+            IRepository<Employee, int> employeeRepository)
             : base(repo, transactionManager)
         {
+            this.salesArticleRepository = salesArticleRepository;
+            this.employeeRepository = employeeRepository;
         }
 
         protected override SaHoldStory CreateFromResource(SaHoldStoryResource resource)
         {
-            throw new System.NotImplementedException();
+            var id = (int)resource.Links.FirstOrDefault(a => a.Rel == "put-on-hold-by")?.Href.ParseId();
+            var test = this.employeeRepository.FindById(
+                id);
+            var holdStory = new SaHoldStory
+                                {
+                                    HoldStoryId = resource.HoldStoryId,
+                                    ArticleNumber = resource.ArticleNumber,
+                                    SalesArticle = this.salesArticleRepository.FindById(resource.ArticleNumber),
+                                    DateStarted = DateTime.Parse(resource.DateStarted),
+                                    PutOnHoldByEmployee = this.employeeRepository.FindById((int)resource.Links.FirstOrDefault(a => a.Rel == "put-on-hold-by")?.Href.ParseId()),
+                                    ReasonStarted = resource.ReasonStarted,
+                                    RootProduct = resource.RootProduct,
+                                    DateFinished = null,
+                                    ReasonFinished = null,
+                                    AnticipatedEndDate = null
+                                };
+            return holdStory;
         }
 
         protected override void UpdateFromResource(SaHoldStory entity, SaHoldStoryResource updateResource)
