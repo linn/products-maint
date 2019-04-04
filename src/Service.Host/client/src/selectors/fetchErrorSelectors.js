2cd1c50284603825cac4cbfb1219def8ea7e1ee6
Peter <Peter.MacDonough@linn.co.uk>
2019-04-04 12:02:05 +0100

    Format error message if multiple errors

--- src/Service.Host/client/src/selectors/fetchErrorSelectors.js
+++ src/Service.Host/client/src/selectors/fetchErrorSelectors.js
@@ -4,8 +4,16 @@
         return null;
     }
 
-    if (fetchError.errors) {
+    if (fetchError.errors.length > 1) {
+        return fetchError.errors.map(error => error.errorMessage).join(', ');
+    }
+
+    if (fetchError.errors[0].errorMessage) {
         return fetchError.errors[0].errorMessage;
+    }
+
+    if (fetchError.errors) {
+        return fetchError.errors[0];
     }
 
     if (fetchError.statusText) {
