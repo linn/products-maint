3a3e191ea873e34ce0242c005e342b5cbac38d0a
lewisre <lewisre@PC1152>
2019-04-05 08:55:24 +0100

    folder rename

---
+++ src/Service.Host/client/src/containers/saHoldStories/SaHoldStory.js
@@ -0,0 +1,30 @@
+import { connect } from 'react-redux';
+import SaHoldStory from '../../components/saHoldStories/SaHoldStory';
+import initialiseOnMount from '../common/functionalInitialiseOnMount';
+import saHoldStorySelectors from '../../selectors/saHoldStorySelectors';
+import saHoldStoryActions from '../../actions/saHoldStoryActions';
+import getSingleErrorMessage from '../../selectors/fetchErrorSelectors';
+
+const mapStateToProps = (state, { match }) => ({
+    item: saHoldStorySelectors.getItem(state),
+    editStatus: 'view',
+    itemId: match.params.holdStoryId || null,
+    loading: saHoldStorySelectors.getLoading(state),
+    errorMessage: getSingleErrorMessage(state),
+    snackbarVisible: saHoldStorySelectors.getSnackbarVisible(state)
+});
+
+const initialise = ({ itemId }) => dispatch => {
+    dispatch(saHoldStoryActions.fetch(itemId));
+};
+
+const mapDispatchToProps = {
+    initialise,
+    setSnackbarVisible: saHoldStoryActions.setSnackbarVisible
+
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(initialiseOnMount(SaHoldStory));
