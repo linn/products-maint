dc71b47b4dd18629f26f3563c8127df377b5deca
Adam Campbell <Adam.Campbell@linn.co.uk>
2019-04-16 09:48:18 +0100

    tidy

--- src/Facade/Services/SerialNumberService.cs
+++ src/Facade/Services/SerialNumberService.cs
@@ -29,47 +29,5 @@
         {
             return serialNumber => serialNumber.SernosNumber.ToString().Equals(searchTerm);
         }
-
-        //        private readonly IRepository<SerialNumber, int> repository;
-        //        private readonly ITransactionManager transactionManager;        
-        //        
-        //        public SerialNumberService(IRepository<SerialNumber, int> repository, ITransactionManager transactionManager)
-        //        {
-        //            this.repository = repository;
-        //            this.transactionManager = transactionManager;
-        //        }
-        //
-        //        public IResult<SerialNumber> GetByTRef(int sernosTRef)
-        //        {
-        //            var serialNumber = this.repository.FindById(sernosTRef);
-        //            if (serialNumber == null)
-        //            {
-        //                return new NotFoundResult<SerialNumber>(sernosTRef.ToString());
-        //            }
-        //
-        //            return new SuccessResult<SerialNumber>(serialNumber);
-        //        }
-        //
-        //        public IResult<IEnumerable<SerialNumber>> GetBySernosNumber(int sernosNumber)
-        //        {
-        //            var serialNumbers = this.repository.FilterBy(s => s.SernosNumber == sernosNumber);
-        //            if (serialNumbers == null)
-        //            {
-        //                return new NotFoundResult<IEnumerable<SerialNumber>>(sernosNumber.ToString());
-        //            }
-        //
-        //            return new SuccessResult<IEnumerable<SerialNumber>>(serialNumbers);
-        //        }
-        //
-        //        public IResult<SerialNumber> Add(SerialNumberResource resource)
-        //        {
-        //            throw new System.NotImplementedException();
-        //        }
-        //
-        //        public IResult<SerialNumber> Update(int sernosTRef, SerialNumberResource resource)
-        //        {
-        //            throw new System.NotImplementedException();
-        //        }
-        //    }
     }
 }
