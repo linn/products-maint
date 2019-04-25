a50ccc70e9aad5b3de750b06fe278d0ecfc90ba5
lewisre <lewisre@PC1152>
2019-04-25 16:55:34 +0100

    new menu idea

--- src/Service.Host/client/src/components/Navbar.js
+++ src/Service.Host/client/src/components/Navbar.js
@@ -1,60 +1,119 @@
 import React, { useState, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+import { Tabs, Tab, AppBar, ClickAwayListener, Menu, MenuItem } from '@material-ui/core';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+import Panel from './Panel';
 
 const styles = {
     root: {
         width: '100%',
         position: 'absolute',
-        top: 0
+        top: 0,
+        zIndex: 10
     },
     tab: {
         minWidth: 20
     },
     tabLabel: {
         fontSize: '12px'
+    },
+    panel: {
+        position: 'relative'
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20
+    },
+    userTab: {
+        float: 'right'
     }
 };
 
-function Navbar({ classes, menu, loading, history }) {
+function Navbar({ classes, menu, loading }) {
+    const { sections } = menu;
+    const menuIds = sections.map(item => item.id);
     const [selected, setSelected] = useState(false);
-
-    const handleChange = (event, value) => {
-        const menuIds = menu.map(item => item.id);
-        if (value || value === 0) {
-            history.push(`/${menuIds[value]}`);
-            setSelected(value);
-        }
+    const [anchorEl, setAnchorEl] = useState();
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget);
     };
+    const handleClose = () => {
+        setAnchorEl();
+    };
+    // history.push(`/${menuIds[value]}`); get history comes in as props
+
     return (
-        <div className={classes.root}>
-            <AppBar position="static" color="default">
-                {menu && !loading ? (
-                    <Tabs
-                        value={selected}
-                        onChange={handleChange}
-                        scrollable
-                        scrollButtons="auto"
-                        indicatorColor="primary"
-                        textColor="primary"
-                    >
-                        {menu.map(item => (
-                            <Tab
-                                key={item.id}
-                                classes={{ root: classes.tab }}
-                                label={<span className={classes.tabLabel}>{item.title}</span>}
-                                selected={false}
+        <ClickAwayListener onClickAway={() => setSelected(false)}>
+            <div className={classes.root}>
+                <AppBar position="static" color="default">
+                    {menu && !loading ? (
+                        <Fragment>
+                            <Tabs
+                                value={selected}
+                                onChange={(event, value) => setSelected(value)}
+                                scrollButtons="auto"
+                                variant="fullWidth"
+                                indicatorColor="primary"
+                                textColor="primary"
+                            >
+                                {sections.map(item => (
+                                    <Tab
+                                        id={item.id}
+                                        key={item.id}
+                                        classes={{ root: classes.tab }}
+                                        label={
+                                            <span className={classes.tabLabel}>{item.title}</span>
+                                        }
+                                        selected={false}
+                                    />
+                                ))}
+                                <Tab
+                                    aria-owns={anchorEl ? 'simple-menu' : undefined}
+                                    onClick={handleClick}
+                                    id={sections.length}
+                                    key={sections.length}
+                                    classes={{ root: classes.tab }}
+                                    label={<AccountCircle />}
+                                    selected={false}
+                                />{' '}
+                            </Tabs>
+                            <Menu
+                                id="simple-menu"
+                                anchorEl={anchorEl}
+                                open={Boolean(anchorEl)}
+                                onClose={handleClose}
+                            >
+                                <MenuItem onClick={handleClose}>{menu.myStuff.userName}</MenuItem>
+                                <Fragment>
+                                    {menu.myStuff.groups.map(group => (
+                                        <a href={group.items[0].href}>
+                                            <MenuItem onClick={handleClose}>
+                                                {group.items[0].title}
+                                            </MenuItem>{' '}
+                                        </a>
+                                    ))}
+                                </Fragment>
+                            </Menu>
+                        </Fragment>
+                    ) : (
+                        <Fragment />
+                    )}
+                </AppBar>
+                {menuIds.map(
+                    (item, i) =>
+                        selected === i && (
+                            <Panel
+                                section={sections.filter(e => e.id === item)[0]}
+                                id={item}
+                                style={{ align: 'right' }}
+                                anchorEl={item.id}
                             />
-                        ))}
-                    </Tabs>
-                ) : (
-                    <Fragment />
+                        )
                 )}
-            </AppBar>
-        </div>
+            </div>
+        </ClickAwayListener>
     );
 }
 
---
+++ src/Service.Host/client/src/components/Panel.js
@@ -0,0 +1,76 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { Paper, Grid, List, ListItem, Typography } from '@material-ui/core';
+
+const styles = {
+    listItemText: {
+        '&:first-child': {
+            paddingLeft: 30,
+            paddingTop: 0,
+            paddingBottom: 0
+        }
+    },
+    paper: {
+        backgroundColor: '#f5f5f5',
+        zIndex: -1
+    },
+    menuItems: {
+        fontSize: '12px',
+        lineHeight: 2
+    }
+};
+
+function Panel({ section, classes }) {
+    const { columns } = section;
+
+    return (
+        <Paper classes={{ root: classes.paper }}>
+            <Grid container justify="left">
+                {columns.map(col => (
+                    <Grid item xs={12} sm={6} md={4} lg={3} xl={3}>
+                        {col.categories.map(category => (
+                            <List>
+                                <ListItem>
+                                    <Typography variant="button" gutterBottom>
+                                        {category.title.replace('&amp;', '&')}
+                                    </Typography>{' '}
+                                </ListItem>
+                                {category.items.map(entry => (
+                                    <Fragment>
+                                        {entry.showInMenu && (
+                                            <a
+                                                href={entry.href}
+                                                style={{ textDecoration: 'none !important' }}
+                                            >
+                                                <ListItem
+                                                    classes={{ root: classes.listItemText }}
+                                                    button
+                                                >
+                                                    <Typography
+                                                        variant="overline"
+                                                        classes={{ overline: classes.menuItems }}
+                                                    >
+                                                        {' '}
+                                                        {entry.title}
+                                                    </Typography>
+                                                </ListItem>
+                                            </a>
+                                        )}{' '}
+                                    </Fragment>
+                                ))}
+                            </List>
+                        ))}
+                    </Grid>
+                ))}
+            </Grid>
+        </Paper>
+    );
+}
+
+Panel.propTypes = {
+    classes: PropTypes.shape({}).isRequired,
+    section: PropTypes.shape({}).isRequired
+};
+
+export default withStyles(styles)(Panel);
