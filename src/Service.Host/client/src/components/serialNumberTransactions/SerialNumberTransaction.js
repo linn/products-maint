4b7546c59aaa1f584ce5fb8663f26cf612b18270
Peter <Peter.MacDonough@linn.co.uk>
2019-05-15 15:45:09 +0100

    Add grid for editing&adding Counts

--- src/Service.Host/client/src/components/serialNumberTransactions/SerialNumberTransaction.js
+++ src/Service.Host/client/src/components/serialNumberTransactions/SerialNumberTransaction.js
@@ -1,6 +1,7 @@
 ﻿﻿import React, { Fragment, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Grid } from '@material-ui/core';
+import { Grid, Button, Table, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
 import {
     SaveBackCancelButtons,
     InputField,
@@ -68,6 +69,21 @@ function SerialNumberTransaction({
         setSerialNumberTransaction({ ...serialNumberTransaction, [propertyName]: newValue });
     };
 
+    const handleNewElement = (propertyName, newValue) => {
+        setEditStatus('edit');
+        // ({ ...newElement, [propertyName]: newValue });
+    };
+
+    const handleElementChange = (propertyName, newValue) => {
+        setEditStatus('edit');
+        const splitIndex = propertyName.indexOf(',');
+        const index = propertyName.slice(0, splitIndex);
+        const prop = propertyName.slice(splitIndex + 1);
+        const { elements } = salesPackage;
+        elements[index][prop] = newValue;
+        // setSalesPackage({ ...salesPackage, elements });
+    };
+
     const yesNoOptions = ['Y', 'N'];
 
     return (
@@ -131,6 +147,133 @@ function SerialNumberTransaction({
                                 propertyName="manualPost"
                             />
                         </Grid>
+                        <Grid container>
+                            <Grid item xs={6}>
+                                <InputField
+                                    value={serialNumberTransaction.comment}
+                                    label="Comment"
+                                    rows={3}
+                                    fullWidth
+                                    onChange={handleFieldChange}
+                                    propertyName="comment"
+                                />
+                            </Grid>
+                        </Grid>
+
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Count</TableCell>
+                                    <TableCell>Count Increment</TableCell>
+                                    <TableCell>Correct Value</TableCell>
+                                    <TableCell>Check Error</TableCell>
+                                    <TableCell>Message</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {serialNumberTransaction.elements &&
+                                    serialNumberTransaction.elements.map((row, index) => (
+                                        <TableRow style={cursor} key={row.count}>
+                                            <TableCell>
+                                                <Dropdown
+                                                    disabled
+                                                    value={row.count}
+                                                    label="Count"
+                                                    propertyName="count"
+                                                />
+                                            </TableCell>
+                                            <TableCell>
+                                                <InputField
+                                                    value={row.countIncrement}
+                                                    label="Count Increment"
+                                                    type="number"
+                                                    onChange={handleElementChange}
+                                                    propertyName={`${index},countIncrement`}
+                                                />
+                                            </TableCell>
+                                            <TableCell>
+                                                <InputField
+                                                    value={row.correctValue}
+                                                    label="Correct Value"
+                                                    onChange={handleElementChange}
+                                                    propertyName={`${index},correctValue`}
+                                                />
+                                            </TableCell>
+                                            <TableCell>
+                                                <Dropdown
+                                                    value={row.checkError}
+                                                    label="Check Error"
+                                                    onChange={handleElementChange}
+                                                    propertyName={`${index},checkError`}
+                                                />
+                                            </TableCell>
+                                            <TableCell>
+                                                <InputField
+                                                    type="text"
+                                                    value={row.message}
+                                                    label="Message"
+                                                    onChange={handleElementChange}
+                                                    propertyName={`${index},message`}
+                                                />
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                <TableRow>
+                                    <TableCell>
+                                        <Button onClick={showFieldsToAddElement}>
+                                            <AddIcon />
+                                        </Button>
+                                    </TableCell>
+                                </TableRow>
+                                {newElements &&
+                                    newElements.map((element, index) => (
+                                        <TableRow style={cursor}>
+                                            <TableCell>
+                                                <Dropdown
+                                                    disabled
+                                                    value={element.count}
+                                                    label="Count"
+                                                    propertyName="count"
+                                                />
+                                            </TableCell>
+                                            <TableCell>
+                                                <InputField
+                                                    value={element.countIncrement}
+                                                    label="Count Increment"
+                                                    type="number"
+                                                    onChange={handleNewElement}
+                                                    propertyName={`${index},countIncrement`}
+                                                />
+                                            </TableCell>
+                                            <TableCell>
+                                                <InputField
+                                                    value={element.correctValue}
+                                                    label="Correct Value"
+                                                    onChange={handleNewElement}
+                                                    propertyName={`${index},correctValue`}
+                                                />
+                                            </TableCell>
+                                            <TableCell>
+                                                <Dropdown
+                                                    value={element.checkError}
+                                                    label="Check Error"
+                                                    onChange={handleNewElement}
+                                                    propertyName={`${index},checkError`}
+                                                />
+                                            </TableCell>
+                                            <TableCell>
+                                                <InputField
+                                                    type="text"
+                                                    value={element.message}
+                                                    label="Message"
+                                                    onChange={handleNewElement}
+                                                    propertyName={`${index},message`}
+                                                />
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                            </TableBody>
+                        </Table>
                         <Grid item xs={12}>
                             <SaveBackCancelButtons
                                 saveDisabled={
