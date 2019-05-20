c80a59322ab1db9c7554e1e143a41955a711dd4e
lewisre <lewisre@PC1152>
2019-05-20 12:30:47 +0100

    remove unused theme stuff

--- src/Service.Host/client/src/components/MenuPage.js
+++ src/Service.Host/client/src/components/MenuPage.js
@@ -1,43 +1,21 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { List, ListItem, Typography, Grid } from '@material-ui/core';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import { createMuiTheme } from '@material-ui/core/styles';
 import Page from '../containers/Page';
-
-const theme = createMuiTheme({
-    overrides: {
-        MuiListSubheader: {
-            root: {
-                fontSize: '32px',
-                color: 'black',
-                padding: '15px'
-            }
-        },
-        MuiListItem: {
-            root: {
-                paddingTop: '0px',
-                paddingBottom: '0px'
-            }
-        }
-    }
-});
 
 const MenuPage = ({ section }) => (
     <Page>
-        <MuiThemeProvider theme={theme}>
-            <Grid container alignItems="flex-start" styles={{ alignItems: 'flex-start' }}>
-                {section !== null ? (
-                    section.map((column, i) => (
-                        <Grid item xs={12} sm={12} md={6} key={i}>
-                            <Column data={column} />
-                        </Grid>
-                    ))
-                ) : (
-                    <Fragment />
-                )}
-            </Grid>
-        </MuiThemeProvider>
+        <Grid container alignItems="flex-start" styles={{ alignItems: 'flex-start' }}>
+            {section !== null ? (
+                section.map((column, i) => (
+                    <Grid item xs={12} sm={12} md={6} key={i}>
+                        <Column data={column} />
+                    </Grid>
+                ))
+            ) : (
+                <Fragment />
+            )}
+        </Grid>
     </Page>
 );
 
