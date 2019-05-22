7039adbeae8da69172cacdf711226dd44c171b68
richard phillips <richard.phillips@linn.co.uk>
2019-05-22 11:53:24 +0100

    import utilities

--- src/Service.Host/client/src/helpers/utilities.js
+++ src/Service.Host/client/src/helpers/utilities.js
@@ -1,16 +1,4 @@
-﻿export const getHref = (itemWithLinks, rel) => {
-    if (itemWithLinks && itemWithLinks.links && itemWithLinks.links.length > 0) {
-        const link = itemWithLinks.links.find(l => l.rel === rel);
-
-        return link ? link.href : null;
-    }
-
-    return null;
-};
-
-export const getSelfHref = itemWithLinks => getHref(itemWithLinks, 'self');
-
-export const makeNumber = (str, defaultValue = 0) => {
+﻿export const makeNumber = (str, defaultValue = 0) => {
     const result = Number(str);
     return Number.isNaN(result) ? defaultValue : result;
 };
