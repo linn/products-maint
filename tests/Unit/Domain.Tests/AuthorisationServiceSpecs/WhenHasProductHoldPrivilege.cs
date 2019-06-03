eb691e5d9314d945963adc6ac833f3394ca2f1ea
lewisre <lewisre@PC1152>
2019-06-03 09:45:15 +0100

    domain tests

---
+++ tests/Unit/Domain.Tests/AuthorisationServiceSpecs/WhenHasProductHoldPrivilege.cs
@@ -0,0 +1,22 @@
+﻿namespace Linn.Products.Domain.Tests.AuthorisationServiceSpecs
+{
+    using FluentAssertions;
+
+    using NUnit.Framework;
+
+    public class WhenHasProductHoldPrivilege : ContextBase
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            this.Privileges.Add("product.hold");
+            this.HasPrivilegeResult = this.Sut.CanPutProductOnOffHold(this.Privileges);
+        }
+
+        [Test]
+        public void ShouldReturnTrue()
+        {
+            this.HasPrivilegeResult.Should().BeTrue();
+        }
+    }
+}
