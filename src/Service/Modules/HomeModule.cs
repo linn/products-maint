46c32c2527451638055ef33e0f50c2a462024741
lewisre <lewisre@PC1152>
2019-06-04 15:39:16 +0100

    comment out the regex monster

--- src/Service/Modules/HomeModule.cs
+++ src/Service/Modules/HomeModule.cs
@@ -10,6 +10,8 @@
         public HomeModule()
         {
             this.Get("/", args => new RedirectResponse("/products/maint"));
+            this.Get("/", args => new RedirectResponse("/products/maint"));
+            this.Get("/products", _ => this.GetApp());
             this.Get("/products/maint", _ => this.GetApp());
             this.Get("/products/reports", _ => this.GetApp());
             this.Get("/products/reports/(.*)/report", _ => this.GetApp());
@@ -19,7 +21,7 @@
             this.Get("/products/maint/signin-oidc-client", _ => this.GetApp());
             this.Get("/products/maint/signin-oidc-silent", _ => this.SilentRenew());
 
-            this.Get(@"^(.*)$", _ => this.GetApp());
+            //this.Get(@"^(.*)$", _ => this.GetApp());
         }
 
         private object SilentRenew()
