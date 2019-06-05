f090c3418bc828ada8dee949c6c3d874ad2fc6ad
lewisre <lewisre@PC1152>
2019-06-05 15:30:39 +0100

    wip

--- src/Service/Modules/HomeModule.cs
+++ src/Service/Modules/HomeModule.cs
@@ -18,10 +18,10 @@
 
             this.Get("/products/maint/(.*)/create", _ => this.GetApp());
 
-            this.Get("/products/maint/signin-oidc-client", _ => this.GetApp());
-            this.Get("/products/maint/signin-oidc-silent", _ => this.SilentRenew());
+            //this.Get("/products/maint/signin-oidc-client", _ => this.GetApp());
+            //this.Get("/products/maint/signin-oidc-silent", _ => this.SilentRenew());
 
-            this.Get(@"^(.*)$", _ => this.GetApp());
+            //this.Get(@"^(.*)$", _ => this.GetApp());
         }
 
         private object SilentRenew()
