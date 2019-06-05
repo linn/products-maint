d5bcde55d9824ab23392b82a8b888d440890c6e1
Adam Campbell <Adam.Campbell@linn.co.uk>
2019-06-05 12:13:47 +0100

    update authorised action

--- src/Domain/AuthorisedAction.cs
+++ src/Domain/AuthorisedAction.cs
@@ -7,5 +7,7 @@ namespace Linn.Products.Domain
         public const string TariffAdmin = "tariff.admin";
 
         public const string ProductHold = "product.hold";
+
+        public const string SerialNumberAdmin = "serial-number-admin";
     }
 }
