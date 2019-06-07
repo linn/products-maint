24c4d9e1dea58f721a74d30dcc64972620121def
Peter <Peter.MacDonough@linn.co.uk>
2019-06-07 09:59:53 +0100

    Fix tests

--- tests/Integration/Service.Tests/TariffModuleSpecs/WhenGettingAllTariffs.cs
+++ tests/Integration/Service.Tests/TariffModuleSpecs/WhenGettingAllTariffs.cs
@@ -13,6 +13,7 @@ namespace Linn.Products.Service.Tests.TariffModuleSpecs
     using Nancy.Testing;
 
     using NSubstitute;
+    using NSubstitute.Core.Arguments;
 
     using NUnit.Framework;
 
@@ -21,18 +22,11 @@ namespace Linn.Products.Service.Tests.TariffModuleSpecs
         [SetUp]
         public void SetUp()
         {
-            var tariff1 = new Tariff
-            {
-                TariffCode = "test",
-                Description = "test-case"
-            };
-            var tariff2 = new Tariff
-            {
-                TariffCode = "test-1",
-                Description = "test-case-1"
-            };
-            this.TariffService.GetAll()
-                .Returns(new SuccessResult<IEnumerable<Tariff>>(new List<Tariff> { tariff1, tariff2 }));
+            var tariff1 = new Tariff { TariffCode = "test", Description = "test-case" };
+            var tariff2 = new Tariff { TariffCode = "test-1", Description = "test-case-1" };
+            this.TariffService.GetAll(Arg.Any<IEnumerable<string>>()).Returns(
+                new SuccessResult<ResponseModel<IEnumerable<Tariff>>>(
+                    new ResponseModel<IEnumerable<Tariff>>(new List<Tariff> { tariff1, tariff2 }, new List<string>())));
 
             this.Response = this.Browser.Get(
                 "/products/maint/tariffs",
@@ -48,13 +42,13 @@ namespace Linn.Products.Service.Tests.TariffModuleSpecs
         [Test]
         public void ShouldCallService()
         {
-            this.TariffService.Received().GetAll();
+            this.TariffService.Received().GetAll(Arg.Any<IEnumerable<string>>());
         }
 
         [Test]
         public void ShouldReturnResource()
         {
-            var resources = this.Response.Body.DeserializeJson<IEnumerable<TariffResource>>().ToList();
+            var resources = this.Response.Body.DeserializeJson<ResponseResource<IEnumerable<TariffResource>>>().ResponseData.ToList();
             resources.Should().HaveCount(2);
             resources.Should().Contain(a => a.TariffCode == "test" && a.Description == "test-case");
             resources.Should().Contain(a => a.TariffCode == "test-1" && a.Description == "test-case-1");
