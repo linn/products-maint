693788d682a248f5f5d7d700c4bc88b0661c0556
lewisre <lewisre@PC1152>
2019-09-10 11:14:22 +0100

    file rename

--- tests/Integration/Service.Tests/StockTriggerLevelReportModuleSpecs/ContextBase.cs
+++ tests/Integration/Service.Tests/StockTriggerLevelReportModuleSpecs/ContextBase.cs
@@ -21,17 +21,21 @@
     {
         protected IStockTriggerLevelsService StockTriggerLevelsService { get; private set; }
 
+        protected IOrdersByNominalService OrdersByNominalService { get; private set; }
+
         [SetUp]
         public void EstablishContext()
         {
             this.StockTriggerLevelsService = Substitute.For<IStockTriggerLevelsService>();
+            this.OrdersByNominalService = Substitute.For<IOrdersByNominalService>();
 
             var bootstrapper = new ConfigurableBootstrapper(
                 with =>
                 {
                     with.Dependency(this.StockTriggerLevelsService);
+                    with.Dependency(this.OrdersByNominalService);
                     with.Dependency<IResourceBuilder<ResultsModel>>(new ResultsModelResourceBuilder());
-                    with.Module<StockTriggerLevelReportModule>();
+                    with.Module<MiscReportsModule>();
                     with.ResponseProcessor<ResultsModelJsonResponseProcessor>();
                     with.RequestStartup(
                         (container, pipelines, context) =>
