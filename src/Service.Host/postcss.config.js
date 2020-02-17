411681b5a042e9ced8dbf556524a8bd8d04e0f8c
lewisre <lewisre@PC1152>
2020-02-17 15:22:45 +0000

    update all the things

--- src/Service.Host/postcss.config.js
+++ src/Service.Host/postcss.config.js
@@ -1,7 +1,5 @@
-﻿module.exports = {
-    plugins: [
-        require('autoprefixer')({
-            browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9']
-        })
-    ]
+﻿const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    plugins: [autoprefixer({})]
 };
