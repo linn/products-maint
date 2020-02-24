64a2e12c2b14c56af6111608c5cd4d8bef50f4fe
Adam Campbell <Adam.Campbell@linn.co.uk>
2020-02-24 10:59:14 +0000

    sales analysis domain

---
+++ src/Domain.Linnapps/SalesAnalysis.cs
@@ -0,0 +1,21 @@
+﻿namespace Linn.Products.Domain.Linnapps
+{
+    using System;
+
+    public class SalesAnalysis
+    {
+        public int SanlId { get; set; }
+
+        public string ArticleNumber { get; set; }
+
+        public string AccountingCompany { get; set; }
+
+        public string Countrycode { get; set; }
+
+        public string DocumentType { get; set; }
+
+        public DateTime SanlDate { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
