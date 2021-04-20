f03a033125bc0cbbc80c9d1d24712d9576ceced9
Iain Crawford <IainC@linn.co.uk>
2021-04-20 12:00:04 +0100

    trying to fix home link by updating components lib and other required npm packages

--- src/Service.Host/client/src/containers/Page.js
+++ src/Service.Host/client/src/containers/Page.js
@@ -1,11 +1,13 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { Page, getRequestErrors } from '@linn-it/linn-form-components-library';
+import config from '../config';
 
 const mapStateToProps = (state, ownProps) => ({
     requestErrors: getRequestErrors(state),
     showRequestErrors: ownProps.showRequestErrors,
-    width: ownProps.width
+    width: ownProps.width,
+    homeUrl: config.appRoot
 });
 
 const mapDispatchToProps = {};
